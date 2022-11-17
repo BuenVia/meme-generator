ed082cab96bb6050180dcdf6b94daafdde3b4f30
SaracensFan <matthewclifford@hotmail.co.uk>
2022-11-17 09:37:19 +0000

    update

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import memeData from "../memeData"
 
 export default function Meme() {
 
+    const [meme, setMeme] = useState({
+        topText: '',
+        bottomText: '',
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = useState(memeData)
+
+
     function handleClick() {
         const rand = Math.floor(Math.random() * 99)
-        console.log(memeData.data.memes[rand]);
+        setMeme(prevVals => {
+            return {
+                ...prevVals,
+                randomImage: allMemeImages.data.memes[rand].url
+            }
+        });
     }
 
-    return <div className="container">
+    return (
+        <div className="container">
             <div className="inputs">
                 <input type='text' className="input" placeholder="Top line" />
                 <input type='text' className="input" placeholder="Bottom line" />
             </div>
             <button onClick={handleClick} className="btn">Get a new meme image <i className="fa-solid fa-file-image"></i></button>
-    </div>
-}
+            <img src={meme.randomImage} alt='Meme' className="memes--image"></img>
+        </div>
+
+)}
